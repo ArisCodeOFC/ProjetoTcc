2beb3e4f052ea3b4f18a3d5d1b56f9a621142b04
Allan Alves <allanalves1904@gmail.com>
2018-07-30 21:48:09 -0300

    Conectando o login com a api

--- js/login-user.js
+++ js/login-user.js
@@ -7,6 +7,33 @@ $(window).on("finishload", function(){
 
         //Esse trecho roda quando o botao for acionado, e pega os dados do formulario
         $('#login-user').submit(function(impedir){
+            //Função para impedir de recaregar a pag
+            impedir.preventDefault();
+
+            //pegando os valores dos campos que o usuario digitou
+
+            //val -> valor que o usuário digitou nas caixas
+            var usuario =  $('#user').val();
+            var senha = $('#pass').val();
+
+            var login_user ={
+                email: usuario, senha: senha
+            };
+
+            //Criando um objeto chamando pela URL
+            $.ajax({
+                //link da api
+                url:"api/v1/usuario/login.php",
+                type:"POST",
+                dataType:"json",
+                data:JSON.stringify(login_user),
+                success:function(){
+                    window.location.href = "index.html";
+                },
+                error:function(erro){
+                    $('#erro').texte(erro.responseText);
+                }
+            });
 
         });
     });
