bffef238ee6e9d3cb19fcb0343cb314890380758
WESLEY ALVES DA SILVA <17170129@CFP127ALUNO.LOCAL>
2018-08-02 15:04:45 -0300

    Editar tarefa

--- js/tarefa.js
+++ js/tarefa.js
@@ -15,42 +15,119 @@ $(window).on("finishload", function() {
             }
         });
 
-        var frm = $("#form-tarefa");
-        frm.submit(function(e) {
+        $("#form-tarefa").submit(function(e) {
             e.preventDefault();
-            var form = $(this);
-            var url = form.attr('action');
-            var tarefa = {titulo: $("#txttitulo").val(), descricao: $("#txtdescricao").val()};
+            if ($(this).data("tarefa")) {
+                editarTarefa($(this).data("tarefa").id);
+            } else {
+                adicionarTarefa();
+            }
 
-            $.ajax({
-                type: frm.attr('method'),
-                url: 'api/v1/tarefa/inserir.php',
-                data: JSON.stringify(tarefa),
-                success: function (data) {
-                    console.log('SUCESSO');
-                    console.log(data);
-                    tarefa.id = data.id;
-                    $(".add_button").trigger("click");
-                    listarTarefa(tarefa).prependTo("#home_block");
-                    form[0].reset();
-                },
-                error: function (data) {
-                    console.log('ERRO');
-                    console.log(data);
-                },
-            });
+            $(this).removeData("tarefa");
+        });
+
+        $("body").on("click", ".editar", function() {
+            var tarefa = $(this).parent().parent().data("tarefa");
+            $("#txttitulo").val(tarefa.titulo);
+            $("#txtdescricao").val(tarefa.descricao);
+            $(".btnenviar").text("Salvar");
+            $("#form-tarefa").data("tarefa", tarefa);
+            $(".btncancelar").show();
+            $(".tarefa_block.content").css("max-height", "350px");
+        });
+
+        $("body").on("click", ".excluir", function() {
+            var id = $(this).parent().parent().attr("data-id");
+            excluirTarefa(id);
+        });
+
+        $(".btncancelar").click(function() {
+            $("#form-tarefa").removeData("tarefa");
+            $("#form-tarefa")[0].reset();
+            $(".btnenviar").text("Enviar");
+            $(".btncancelar").hide();
+            $(".tarefa_block.content").css("max-height", "0");
+        });
+
+        $(".add_button").click(function(event) {
+            $("#form-tarefa").removeData("tarefa");
+            $("#form-tarefa")[0].reset();
+            $(".btncancelar").hide();
+            $(".btnenviar").text("Enviar");
+            $(".tarefa_block.content").css("max-height", "350px");
         });
     });
 });
 
 function listarTarefa(tarefa) {
-    var div = $("<div class='tarefa_block'>");
+    var div = $("<div class='tarefa_block'>").attr("data-id", tarefa.id);
     var h2 = $("<h2>").text(tarefa.titulo);
     var p = $("<p>").text(tarefa.descricao);
     var divImagens = $("<div>");
-    $("<figure>").html("<img src='edit.svg'>").appendTo(divImagens);
-    $("<figure>").html("<img src='delete.svg'>").appendTo(divImagens);
+    $("<figure class='editar'>").html("<img src='edit.svg'>").appendTo(divImagens);
+    $("<figure class='excluir'>").html("<img src='delete.svg'>").appendTo(divImagens);
     div.append(h2).append(p).append(divImagens);
     div.data("tarefa", tarefa);
     return div;
+}
+
+function adicionarTarefa() {
+    var form = $("#form-tarefa");
+    var url = form.attr('action');
+    var tarefa = {titulo: $("#txttitulo").val(), descricao: $("#txtdescricao").val()};
+
+    $.ajax({
+        type: form.attr('method'),
+        url: 'api/v1/tarefa/inserir.php',
+        data: JSON.stringify(tarefa),
+        success: function (data) {
+            console.log('SUCESSO');
+            console.log(data);
+            tarefa.id = data.id;
+            $(".tarefa_block.content").css("max-height", "0");
+            listarTarefa(tarefa).prependTo("#home_block");
+            form[0].reset();
+        },
+        error: function (data) {
+            console.log('ERRO');
+            console.log(data);
+        },
+    });
+}
+
+function editarTarefa(idTarefa) {
+    var form = $("#form-tarefa");
+    var tarefa = {id: idTarefa, titulo: $("#txttitulo").val(), descricao: $("#txtdescricao").val()};
+
+    $.ajax({
+        type: "POST",
+        url: "api/v1/tarefa/atualizar.php",
+        data: JSON.stringify(tarefa),
+        dataType: "json",
+        success: function() {
+            $(".tarefa_block[data-id='" + tarefa.id + "']").replaceWith(listarTarefa(tarefa));
+            $(".tarefa_block.content").css("max-height", "0");
+            form[0].reset();
+        },
+
+        error: function(erro) {
+            console.log("Erro ao atualizar tarefa", erro.responseText);
+        }
+    });
+}
+
+function excluirTarefa(idTarefa) {
+    $.ajax({
+        type: "POST",
+        url: "api/v1/tarefa/excluir.php",
+        data: JSON.stringify({id: idTarefa}),
+        dataType: "json",
+        success: function() {
+            $(".tarefa_block[data-id='" + idTarefa + "']").remove();
+        },
+
+        error: function(erro) {
+            console.log("Erro ao excluir tarefa", erro.responseText);
+        }
+    });
 }
